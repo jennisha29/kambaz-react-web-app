5af8f87367e262a3744c06f1ae3450693ec81cde
Jennisha Christina Martin <jennishachristinamartin@Jennishas-MacBook-Pro.local>
2025-01-21 16:26:38 -0500

    second commit

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,6 +7,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kambaz">Kambaz</Link></li>
+        <li><a href="https://github.com/jennisha29?tab=repositories" target="_blank">My GitHub Repository</a></li>
       </ul>
     );
   }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,14 +6,17 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
-      </Routes>
+        <h1>Jennisha Christina Martin</h1>
+        <h2>CS5610 Web Development SEC 01 Spring 2024</h2>
+        <hr/>
+        <h1>Labs</h1>
+        <TOC />
+        <Routes>
+            <Route path="/" element={<Navigate to="Lab1" />} />
+            <Route path="Lab1" element={<Lab1 />} />
+            <Route path="Lab2/*" element={<Lab2 />} />
+            <Route path="Lab3" element={<Lab3 />} />
+        </Routes>
       
     </div>
 );}
