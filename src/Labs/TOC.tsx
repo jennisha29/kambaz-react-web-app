9a36afb116f3f63a8be9cbbb69ad21f2434bb9d2
Jennisha Christina Martin <jennishachristinamartin@Jennishas-MacBook-Pro.local>
2025-02-05 23:27:17 -0500

    a2 CSS and Bootstrap

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
           <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="https://github.com/jennisha29/kambaz-react-web-app.git">My GitHub</Nav.Link>
+          <Nav.Link href="https://github.com/jennisha29/kambaz-react-web-app/tree/a2">My GitHub</Nav.Link>
         </Nav.Item>
       </Nav>
    );}
